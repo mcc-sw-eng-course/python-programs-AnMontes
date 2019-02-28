901cab816765e9d57b4c8ffe3bed449a41d6708c
LAPD <luis.30n@hotmail.com>
2019-02-28 10:41:08 -0600

    Test SortLargeFile

--- L6/test_SortLargeFile.py
+++ L6/test_SortLargeFile.py
@@ -4,12 +4,49 @@ import SortLargeFile
 
 class TestSortLargeFile(unittest.TestCase):
 
-    def test_set_input_data(self):
+    def test_set_input_data_filename(self):
         obj = SortLargeFile.SortLargeCsv()
         with self.assertRaises(ValueError):
             obj.set_input_data(235, ',')
 
+    def test_set_input_data_delimiter(self):
+        obj = SortLargeFile.SortLargeCsv()
         with self.assertRaises(ValueError):
-            obj.set_input_data("")
+            obj.set_input_data("large_csv.csv",'w')
 
+    def test_set_input_data_exist(self):
+        obj = SortLargeFile.SortLargeCsv()
+        with self.assertRaises(Exception):
+            obj.set_input_data("Hola",',')
 
+    def test_set_input_data_array(self):
+        obj = SortLargeFile.SortLargeCsv()
+        obj.set_input_data("large_csv - copia.csv", ',')
+        self.assertEqual(obj.data_array, [5,4,3,2])
+
+    def test_set_output_data_not_input_state(self):
+        obj = SortLargeFile.SortLargeCsv()
+        with self.assertRaises(Exception):
+            obj.set_output_data("large_csv.csv",',')
+
+    def test_set_output_data_string(self):
+        obj = SortLargeFile.SortLargeCsv()
+        obj.set_input_data("large_csv - copia.csv", ',')
+        with self.assertRaises(ValueError):
+            obj.set_output_data(200, ',')
+
+    def test_set_output_data_string(self):
+        obj = SortLargeFile.SortLargeCsv()
+        obj.set_input_data("large_csv - copia.csv", ',')
+        obj.set_output_data("outputcsv.csv", ',')
+
+    def test_execute_merge_sort(self):
+        obj = SortLargeFile.SortLargeCsv()
+        with self.assertRaises(Exception):
+            obj.execute_merge_sort()
+
+    def test_merge_sort(self):
+        obj = SortLargeFile.SortLargeCsv()
+        obj.set_input_data("large_csv - copia.csv", ',')
+        obj.merge_sort(obj.data_array)
+        self.assertEqual(obj.data_array,[2,3,4,5])
