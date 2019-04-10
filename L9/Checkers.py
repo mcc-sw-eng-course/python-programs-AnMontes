c01b7e04f5862962d43a46c0ea07109febf0ffb9
antoniomontesdeocagomez <an.montes95@hotmail.com>
2019-04-09 23:12:11 -0500

    beta 2 checkers, con comentarios

--- L9/Checkers.py
+++ L9/Checkers.py
@@ -20,6 +20,13 @@ class Window(QtWidgets.QWidget):
     def init_ui(self):
         self.l_empty_space = QtWidgets.QLabel("            ")
 
+        # b = BOTON
+        # b1_2 = BOTON en ROW 1, COLUMN 2
+        # ROW, se cuentan desde la parte de abajo de la ventana para arriba en orden ascendente.
+        # COLUMN, se cuenta de izquierda a derecha en orden ascendente.
+        # En general ignora casi todo este metodo y vete hasta la ultima parte de este metodo.
+
+
         self.b1_1 = QtWidgets.QPushButton(self.grep_checker)
         self.b1_2 = QtWidgets.QPushButton(self.grep_checker)
         self.b1_3 = QtWidgets.QPushButton(self.grep_checker)
@@ -164,6 +171,13 @@ class Window(QtWidgets.QWidget):
         self.setWindowTitle("Checkers")
 
         # Do whatever is inside the connect.
+
+        # AQUI SE PONEN TODOS LOS METODOS DE LOS BOTONES!!!!
+        # De aqui para abajo es donde se tienen que agregar el resto de los botones.
+        # Comienza implementando los botones de la mitad para que los puedas ir probando.
+        # Si empiezas por la row 1 u 8 no vas a poder probar su movimientos bien.
+        # Por eso no implemente por completo b1_1.
+
         self.b1_1.clicked.connect(self.btn_selected1_1)
         self.b3_1.clicked.connect(self.btn_selected3_1)
         self.b3_2.clicked.connect(self.btn_selected3_2)
@@ -190,14 +204,19 @@ class Window(QtWidgets.QWidget):
         # self.b1_1.setStyleSheet("")
 
     def btn_selected3_1(self):      # Fully implemented.
-        if self.toggle_selection:
+        if self.toggle_selection:   # Toggle select indica si hay un boton en rojo o no.
+            # Indica si ya hay una casilla cargada en self.curr_selected_btn_cord.
+
             # Allow to be move to if this btn is empty.
             if self.b3_1.text() == self.empty_btn:
                 if self.store_checker == self.grep_checker:     # If a grep was selected.
+                    # Movimiento de casilla que es SIN COMER FICHAS:
                     if self.curr_selected_btn_coord[0] == 2 and self.curr_selected_btn_coord[1] == 1:
                         self.b3_1.setText(self.store_checker)
                         self.clear_btn_checker()
                         self.reset_btns_state()
+                    # Movimiento que involucra comer fichas. 
+                    # Importante notar que esta desfasado dos casillas en rows y en columns
                     elif self.curr_selected_btn_coord[0] == 1 and self.curr_selected_btn_coord[1] == 2 \
                         and self.b2_1.text() == self.line_checker:
                         self.b3_1.setText(self.store_checker)
@@ -205,6 +224,8 @@ class Window(QtWidgets.QWidget):
                         self.clear_btn_checker()
                         self.reset_btns_state()
                         self.grep_score += 1
+                    
+                    # Si se eligieron casillas muy distantes:
                     else:
                         self.l_prompt_message.setText("Checker progresssion is incorrect.")
                         self.reset_btns_state()
@@ -228,41 +249,58 @@ class Window(QtWidgets.QWidget):
                 self.l_prompt_message.setText("This button isn't empty")
                 self.reset_btns_state()
         else:
+            # Si es la primera seleccion de boton a mover
             if not self.b3_1.text() == self.empty_btn:
                 self.l_prompt_message.setText("Game running normal")
+                # Marcar el boton en rojo.
+                # Da apariencia de que esta seleccionado este boton
                 self.b3_1.setStyleSheet("background-color: rgb(255,0,0)")
+                # Cargar en el array cual boton esta seleccionado actualmente.
+                # SE REALIZA EL MOVIMIENTO HASTA QUE SE SELECCIONA OTRO BOTON DESPUES DE ENTRAR A ESTA RUTINA.
                 self.curr_selected_btn_coord[0] = 3
                 self.curr_selected_btn_coord[1] = 1
+                # Guardar cual es el checker que se movera en un futuro.
                 self.store_checker = self.b3_1.text()
+                # Activar las rutinas de la parte de arriba al seleccionar otro boton.
                 self.toggle_selection = not self.toggle_selection
             else:
+                # Si se selecciono un boton que no tiene texto (i.e. sin un checker)
                 self.l_prompt_message.setText("Select a button that has a checker")
                 self.reset_btns_state()
 
     def btn_selected3_2(self):      # Fully implemented.
         if self.toggle_selection:   # If a btn has been previously pressed.
             # Allow to be move to if this btn is empty.
+            # Modificaras aqui:
             if self.b3_2.text() == self.empty_btn:
                 if self.store_checker == self.grep_checker:     # If a grep was selected.
+                    # Modificaras aqui:
                     if self.curr_selected_btn_coord[0] == 2 and \
                             (self.curr_selected_btn_coord[1] == 1 or self.curr_selected_btn_coord[1] == 2):
                         # Checking for forward movement...
+                        # Modificaras aqui:
                         self.b3_2.setText(self.store_checker)
                         self.clear_btn_checker()
                         self.reset_btns_state()
                     # CHECAR SI VA A COMER
+                    # Modificaras aqui:
                     elif self.curr_selected_btn_coord[0] == 1 and self.curr_selected_btn_coord[1] == 1 \
                             and self.b2_1.text() == self.line_checker:
+                        # Modificaras aqui:
                         self.b3_2.setText(self.store_checker)
                         # Limpiar el checker que se comio.
+                        # Modificaras aqui:
                         self.b2_1.setText(self.empty_btn)
                         self.clear_btn_checker()
                         self.reset_btns_state()
                         self.grep_score += 1
+                    # Modificaras aqui:
                     elif self.curr_selected_btn_coord[0] == 1 and self.curr_selected_btn_coord[1] == 3 \
                             and self.b2_2.text() == self.line_checker:
+                        # Modificaras aqui:
                         self.b3_2.setText(self.store_checker)
                         # Limpiar el checker que se comio.
+                        # Modificaras aqui:
                         self.b2_2.setText(self.empty_btn)
                         self.clear_btn_checker()
                         self.reset_btns_state()
@@ -272,23 +310,30 @@ class Window(QtWidgets.QWidget):
                             "Checker progresssion is incorrect.")
                         self.reset_btns_state()
                 elif self.store_checker == self.line_checker:   # If a line was selected.
+                    # Modificaras aqui:
                     if self.curr_selected_btn_coord[0] == 4 \
                             and (self.curr_selected_btn_coord[1] == 1 or self.curr_selected_btn_coord[1] == 2):
                         # Checking for backward movement...
+                        # Modificaras aqui:
                         self.b3_2.setText(self.store_checker)
                         self.clear_btn_checker()
                         self.reset_btns_state()
+                    # Modificaras aqui:
                     elif self.curr_selected_btn_coord[0] == 4 and self.curr_selected_btn_coord[1] == 1 \
                             and self.b5_1.text() == self.grep_checker:
+                        # Modificaras aqui:
                         self.b5_2.setText(self.store_checker)
+                        # Modificaras aqui:
                         self.b4_1.setText(self.empty_btn)
                         self.clear_btn_checker()
                         self.reset_btns_state()
                         self.line_score += 1
-
+                    # Modificaras aqui:
                     elif self.curr_selected_btn_coord[0] == 4 and self.curr_selected_btn_coord[1] == 3 \
                             and self.b5_2.text() == self.grep_checker:
+                        # Modificaras aqui:
                         self.b5_2.setText(self.store_checker)
+                        # Modificaras aqui:
                         self.b4_2.setText(self.empty_btn)
                         self.clear_btn_checker()
                         self.reset_btns_state()
@@ -302,10 +347,14 @@ class Window(QtWidgets.QWidget):
                 self.reset_btns_state()
         else:
             # If the btn initially selected is empty.
+            # Modificaras aqui:
             if not self.b3_2.text() == self.empty_btn:
                 self.l_prompt_message.setText("Game running normal")
+                # Modificaras aqui:
                 self.b3_2.setStyleSheet("background-color: rgb(255,0,0)")
+                # Modificaras aqui:
                 self.store_checker = self.b3_2.text()
+                # Modificaras aqui:
                 self.curr_selected_btn_coord[0] = 3
                 self.curr_selected_btn_coord[1] = 2
                 self.toggle_selection = not self.toggle_selection
@@ -692,12 +741,9 @@ class Window(QtWidgets.QWidget):
         self.toggle_selection = False
 
 
-# TODO: Check if lines can move as intended. IMPLEMENTED BUT NEEDS TESTING.
-# TODO: Check if btn that is going to move to is occupied. IMPLEMENTED BUT NEEDS TESTING.
-# TODO: Eat checkers.
+
 # TODO: Method for continously checking score.
 # TODO: PLAYER TURNS.
-# TODO: Colgar ropa.
 
 app = QtWidgets.QApplication(sys.argv)
 a_window = Window()
